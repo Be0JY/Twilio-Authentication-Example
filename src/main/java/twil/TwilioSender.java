7d012cdfafb64c06db89a4d1296a67a928111ebc
BuildTools <unconfigured@null.spigotmc.org>
2018-10-07 14:21:51 -0400

    finished twilio messaging

--- src/main/java/twil/TwilioSender.java
+++ src/main/java/twil/TwilioSender.java
@@ -5,27 +5,30 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.apache.commons.lang3.RandomStringUtils;
 
-public class main {
+public class TwilioSender {
+    static String generatedString = "";
+
     // Find your Account Sid and Token at twilio.com/user/account
     public static String randomString() {
 
         int length = 5;
         boolean useLetters = true;
         boolean useNumbers = false;
-        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
 
         return generatedString;
     }
 
     public static final String ACCOUNT_SID = "ACe332c51268b739eee6da38239c195b36";
-    public static final String AUTH_TOKEN = "ac0d0eb53faaabc236194df2bdcffbf6";
+    public static final String AUTH_TOKEN = secrets.key;
 
-    public static void main(String[] args) {
+    public static void sendText(String phoneNumber) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
+        String rand_string = randomString();
         Message message = Message
-                .creator(new PhoneNumber("4073356371"), new PhoneNumber("14075430148"),
-                        "Ahoy from Twilio! " + randomString()).create();
+                .creator(new PhoneNumber(phoneNumber), new PhoneNumber("14075430148"),
+                        "Ahoy from Twilio! " + rand_string).create();
 
         System.out.println(message.getSid());
     }
--- src/main/java/twil/fx.java
+++ src/main/java/twil/fx.java
@@ -6,7 +6,9 @@ package twil;
         import javafx.event.EventHandler;
         import javafx.scene.Scene;
         import javafx.scene.control.Button;
+        import javafx.scene.control.TextField;
         import javafx.scene.layout.StackPane;
+        import javafx.scene.layout.VBox;
         import javafx.stage.Stage;
 
 public class fx extends Application {
@@ -16,19 +18,42 @@ public class fx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        primaryStage.setTitle("numberInput");
 
+
+        TextField field = new TextField();
+        TextField field2 = new TextField();
+
+
+        Button btn = new Button("Go");
+        Button btnCheck = new Button("Verify");
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                String num = field.getText();
+                TwilioSender.sendText(num);
+            }
+        });
+
+        btnCheck.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String secretCode = field2.getText();
+                if(secretCode.equals(TwilioSender.generatedString) && !secretCode.equals(""))
+                    System.out.println("Login Success!");
+                else
+                    System.out.println(secretCode + " does not equal " + TwilioSender.generatedString);
             }
         });
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox vb = new VBox();
+
+        vb.getChildren().addAll(btn, field,field2,btnCheck);
+
+        root.getChildren().add(vb);
+
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }

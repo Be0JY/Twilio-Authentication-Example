0fd774eb44d580b0a921e486611198d8741afb35
Elias Valle <therealpeak17@gmail.com>
2018-10-13 20:33:50 -0400

    Core GUI elements added
    Took the original twilio application and added user friendly GUI, and multiple scenes.

--- src/main/java/twil/fx.java
+++ src/main/java/twil/fx.java
@@ -7,11 +7,12 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.text.*;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
 
 public class fx extends Application {
     public static void main(String[] args) {
@@ -20,55 +21,131 @@ public class fx extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        primaryStage.setTitle("numberInput");
-        StackPane root = new StackPane();
-        StackPane root2 = new StackPane();
-        StackPane root3 = new StackPane();
+        primaryStage.setTitle("2FA Concept Demo");
+        primaryStage.getIcons().add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/NSU_1960_Logo.svg/220px-NSU_1960_Logo.svg.png"));
+        //Initialzing the Scencs
+        GridPane gridPane = new GridPane();
+        GridPane gridPane2 = new GridPane();
+        GridPane gridPane3 = new GridPane();
+
+        //--------beginning of first scene
 
 
-        VBox vb = new VBox();
-        VBox vb2 = new VBox();
-        VBox vb3 = new VBox();
-
-        Button btn = new Button("Go");
-        Button btnCheck = new Button("Verify");
+        Text phonetxt = new Text("Phone Number :");
+        Button btnSubmit = new Button("Send Key");
 
         TextField field = new TextField();
-        TextField field2 = new TextField();
-        Text t = new Text();
-        t.setFont(new Font(20));
-        t.setText("Login Success");
 
-        vb.getChildren().addAll(btn, field);
-        vb2.getChildren().addAll(field2, btnCheck);
-        vb3.getChildren().addAll(t);
 
-        root.getChildren().add(vb);
-        root2.getChildren().add(vb2);
-        root3.getChildren().add(vb3);
 
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
 
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        gridPane.setMinSize(400,200);
+
+        gridPane.setPadding(new Insets(10,10,10,10));
+
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        gridPane.add(phonetxt,0,0);
+        gridPane.add(field,1,0);
+        gridPane.add(btnSubmit, 0, 1);
+
+
+
+        btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String num = field.getText();
                 TwilioSender.sendText(num);
-                primaryStage.setScene(new Scene(root2, 300, 250));
+                primaryStage.setScene(new Scene(gridPane2, 400, 200));
+
             }
         });
 
-        btnCheck.setOnAction(new EventHandler<ActionEvent>() {
+// *************start second screaft button condition is met
+
+
+        Button btn = new Button("Verify");
+        TextField field2 = new TextField();
+        Text keyText = new Text("Enter Key Recived:");
+
+
+
+        gridPane2.setMinSize(400,200);
+
+        gridPane2.setPadding(new Insets(10,10,10,10));
+
+        gridPane2.setVgap(5);
+        gridPane2.setHgap(5);
+
+        gridPane2.setAlignment(Pos.CENTER);
+
+        gridPane2.add(keyText,0,0);
+        gridPane2.add(field2,1,0);
+        gridPane2.add(btn, 0, 1);
+
+
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String secretCode = field2.getText();
                 if(secretCode.equals(TwilioSender.generatedString) && !secretCode.equals(""))
-                    primaryStage.setScene(new Scene(root3, 300, 250));
+                    primaryStage.setScene(new Scene(gridPane3, 400, 200));
                 else
                     System.out.println(secretCode + " does not equal " + TwilioSender.generatedString);
             }
         });
+
+
+//-----------Begining of third scene
+
+
+        Text t = new Text();
+        t.setFont(new Font(20));
+
+        t.setText("Login Success");
+        Button btnAgain = new Button("See Demo Again");
+
+
+
+        gridPane3.setMinSize(400,200);
+
+        gridPane3.setPadding(new Insets(10,10,10,10));
+
+        gridPane3.setVgap(5);
+        gridPane3.setHgap(5);
+
+        gridPane3.setAlignment(Pos.CENTER);
+
+
+        gridPane3.add(t,1,0);
+        gridPane3.add(btnAgain,1,2);
+
+        btnAgain.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+
+                start(primaryStage);
+
+            }
+        });
+
+//***^^^ End of thrid scence
+
+
+        //** finishing the creation of scene one
+
+
+
+        Scene scene = new Scene(gridPane);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
 
 
     }
